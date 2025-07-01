80361628de2b3449e40b1ce1f51686d856c924e7
Surya Vemulapalli <iciclecrash50@gmail.com>
2025-07-01 11:42:13 -0400

    Fixed some issues with the infrastructure

--- src/components/Component4.tsx
+++ src/components/Component4.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 
+
 export default function Component4 () {
     return (
         <div className="bg-green-500 rounded-lg p-4 shadow-lg">
-            <p className="bg-green-500">Hello</p>
+            <p className="text-lg text-gr">Hello</p>
         </div> 
     )
 }
